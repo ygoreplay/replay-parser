6f8fe75739f39121c846dba446cb2ba6151bcc10
Sophia <beyondthesophia@gmail.com>
2022-08-05 13:44:19 +0900

    make parser generate data only unique and popular decks

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ async function main() {
             .filter(line => line.startsWith("!setname "))
             .map(line => {
                 const [, value, ...rest] = line.split(" ");
-                return [parseInt(value, 16), rest.join(" ")];
+                return [parseInt(value, 16), rest.join(" ").replace(/.*?\(정발명:(.*?)\).*?$/, "$1")];
             }),
     );
 
@@ -96,58 +96,80 @@ async function main() {
 
     progressBar.terminate();
 
-    const codeNames: string[] = [];
-    const resultData = deckBuffer.map((deck): ResultData => {
-        const allCards = [...deck.main, ...deck.extra];
-        const allSetCodes = _.chain(allCards).map("setcodes").flattenDeep().value();
-        const topCodes = _.chain(allSetCodes)
-            .countBy(c => c)
-            .entries()
-            .orderBy(p => p[1], "desc")
-            .map(p => [parseInt(p[0], 10), p[1]])
-            .slice(0, 3)
-            .value();
+    let codeNames: string[] = [];
+    const resultData = deckBuffer
+        .map((deck): ResultData | null => {
+            const allCards = [...deck.main, ...deck.extra];
+            const allSetCodes = _.chain(allCards).map("setcodes").flattenDeep().value();
+            const topCodes = _.chain(allSetCodes)
+                .countBy(c => c)
+                .entries()
+                .orderBy(p => p[1], "desc")
+                .map(p => [parseInt(p[0], 10), p[1]])
+                .slice(0, 3)
+                .value();
 
-        codeNames.push(codeNameMap[topCodes[0][0]]);
+            if (topCodes.length <= 0) {
+                return null;
+            }
 
-        return {
-            allCount: deck.main.length + deck.extra.length,
-            mainCount: deck.main.length,
-            extraCount: deck.extra.length,
-            monsterCount: deck.main.filter(c => c.isMonster).length,
-            spellCount: deck.main.filter(c => c.isSpell).length,
-            trapCount: deck.main.filter(c => c.isTrap).length,
-            fusionCount: deck.extra.filter(c => c.isFusion).length,
-            synchroCount: deck.extra.filter(c => c.isSynchro).length,
-            xyzCount: deck.extra.filter(c => c.isXYZ).length,
-            pendulumCount: deck.extra.filter(c => c.isPendulum).length,
-            ritualCount: deck.main.filter(c => c.isRitual).length,
-            linkCount: deck.extra.filter(c => c.isLink).length,
-            setCodeRank: topCodes.map(p => p[0]),
-            setCodeCountRank: topCodes.map(p => p[1]),
-        };
-    });
+            codeNames.push(codeNameMap[topCodes[0][0]]);
 
-    const minimalData = resultData.map(item => [
-        item.allCount,
-        item.mainCount,
-        item.extraCount,
-        item.monsterCount,
-        item.spellCount,
-        item.trapCount,
-        item.fusionCount,
-        item.synchroCount,
-        item.xyzCount,
-        item.pendulumCount,
-        item.linkCount,
-        item.ritualCount,
-        item.setCodeRank[0] || 0,
-        item.setCodeCountRank[0] || 0,
-        item.setCodeRank[1] || 0,
-        item.setCodeCountRank[1] || 0,
-        item.setCodeRank[2] || 0,
-        item.setCodeCountRank[2] || 0,
-    ]);
+            return {
+                allCount: deck.main.length + deck.extra.length,
+                mainCount: deck.main.length,
+                extraCount: deck.extra.length,
+                monsterCount: deck.main.filter(c => c.isMonster).length,
+                spellCount: deck.main.filter(c => c.isSpell).length,
+                trapCount: deck.main.filter(c => c.isTrap).length,
+                fusionCount: deck.extra.filter(c => c.isFusion).length,
+                synchroCount: deck.extra.filter(c => c.isSynchro).length,
+                xyzCount: deck.extra.filter(c => c.isXYZ).length,
+                pendulumCount: deck.extra.filter(c => c.isPendulum).length,
+                ritualCount: deck.main.filter(c => c.isRitual).length,
+                linkCount: deck.extra.filter(c => c.isLink).length,
+                setCodeRank: topCodes.map(p => p[0]),
+                setCodeCountRank: topCodes.map(p => p[1]),
+            };
+        })
+        .filter((item): item is ResultData => Boolean(item));
+
+    const topUsedCodeNames = _.chain(codeNames)
+        .countBy(c => c)
+        .entries()
+        .orderBy(p => p[1], "desc")
+        .slice(0, 10)
+        .map(p => p[0])
+        .value();
+
+    let minimalData = _.chain(resultData)
+        .map(item => [
+            item.allCount,
+            item.mainCount,
+            item.extraCount,
+            item.monsterCount,
+            item.spellCount,
+            item.trapCount,
+            item.fusionCount,
+            item.synchroCount,
+            item.xyzCount,
+            item.pendulumCount,
+            item.linkCount,
+            item.ritualCount,
+            item.setCodeRank[0] || 0,
+            item.setCodeCountRank[0] || 0,
+            item.setCodeRank[1] || 0,
+            item.setCodeCountRank[1] || 0,
+            item.setCodeRank[2] || 0,
+            item.setCodeCountRank[2] || 0,
+            codeNameMap[item.setCodeRank[0]],
+        ])
+        .filter(i => topUsedCodeNames.includes(i.at(-1) as string))
+        .uniqBy(i => i.join(", "))
+        .value();
+
+    codeNames = minimalData.map(item => item.at(-1) as string);
+    minimalData = minimalData.map(data => data.slice(0, data.length - 2));
 
     await fs.writeFile("./output.json", JSON.stringify(minimalData));
     await fs.writeFile("./names.txt", codeNames.join("\n"));
